3277469b8fc3492efaf97b48be3f7d17fa062aa9
gaok <dlrkdhr321@gmail.com>
2022-06-28 14:40:04 +0900

    StaticImage 추가(index.js)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/Layout";
 
@@ -5,6 +6,11 @@ import Layout from "../components/Layout";
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford.jpg"
+      />
+      {/* StaticImage를 사용하면 디바이스에 따라 최적화된 이미지를 제공 */}
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
