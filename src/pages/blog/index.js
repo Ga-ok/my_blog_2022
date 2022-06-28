711998d8798b09db60df483ceeeca8250d879082
gaok <dlrkdhr321@gmail.com>
2022-06-28 20:10:55 +0900

    Blog 글목록에 Link 추가

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,35 +1,35 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import { graphql, useStaticQuery } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import { graphql, Link } from "gatsby";
 
-function BlogPage() {
-  const data = useStaticQuery(graphql`
-    query {
-      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-        nodes {
-          frontmatter {
-            date(formatString: "MMMM D, YYYY")
-            title
-          }
-          id
-          body
-        }
-      }
-    }
-  `);
-
+function BlogPage({ data }) {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        slug
+      }
+    }
+  }
+`;
 
 export default BlogPage;
